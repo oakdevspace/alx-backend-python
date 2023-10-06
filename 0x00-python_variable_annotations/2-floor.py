6a603ee43cbc87b327213284a1f4e1c8cde6b830
Kabiru Umar <111059385+oakdevspace@users.noreply.github.com>
2023-10-06 02:06:20 +0100

    2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+returns the floor of the float.
+'''
+
+
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return int(a)
