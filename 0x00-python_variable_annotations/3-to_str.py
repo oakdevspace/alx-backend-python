55ce320fe71d1b7fad34fac7e68dc9512eb067a2
Kabiru Umar <111059385+oakdevspace@users.noreply.github.com>
2023-10-06 02:08:17 +0100

    3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+returns the string representation of the float.
+'''
+
+
+def to_str(n: float) -> str:
+    '''Converts a floating-point number to a string.
+    '''
+    return str(n)
